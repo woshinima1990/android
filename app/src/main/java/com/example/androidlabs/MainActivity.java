7f4774dfd49682e9f9135c646ccf0750ad8861d3
woshinima1990 <rexzyt@hotmail.com>
2020-01-23 16:45:22 -0800

    Added 3 layout for lab 2

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -3,12 +3,100 @@ package com.example.androidlabs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.material.snackbar.Snackbar;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_main_relative);
+
+
+        TextView textView = findViewById(R.id.textView);
+
+        EditText editText = findViewById(R.id.editText);
+
+        Button btn = findViewById(R.id.button);
+
+        btn.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this,getResources().getString(R.string.toast_message),Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        final Switch s = findViewById(R.id.switch1);
+        s.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged (CompoundButton cb, final boolean isChecked){
+                if (isChecked == true){
+                    Snackbar sb = Snackbar.make(cb,getResources().getString(R.string.switch_on), Snackbar.LENGTH_LONG);
+                    sb.setAction("undo", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View click) {
+                            s.setChecked(!isChecked);
+
+                        }
+                    });
+                    sb.show();
+                }
+                else{
+
+                    Snackbar sb = Snackbar.make(cb, getResources().getString(R.string.switch_off), Snackbar.LENGTH_LONG);
+                    sb.setAction("undo", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View click) {
+                            s.setChecked(!isChecked);
+                        }
+                    });
+                    sb.show();
+                }
+            }
+        });
+
+
+        CheckBox cb = findViewById(R.id.checkbox);
+        cb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+
+            @Override
+            public void onCheckedChanged (CompoundButton cb, final boolean isChecked){
+                if (isChecked== true){
+                    Snackbar sb = Snackbar.make(cb, getResources().getString(R.string.check_on), Snackbar.LENGTH_LONG);
+                    sb.setAction("undo", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View click) {
+                            s.setChecked(!isChecked);
+                        }
+                    });
+                    sb.show();
+                }
+                else{
+
+                    Snackbar sb = Snackbar.make(cb,getResources().getString(R.string.check_off), Snackbar.LENGTH_LONG);
+                    sb.setAction("undo", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View click) {
+                            s.setChecked(!isChecked);
+                        }
+                    });
+                    sb.show();
+                }
+            }
+        });
     }
+
 }
